8691226d4f252256798cf986f888fc8fbe9c356f
Marine Richini <marine.richini@gmail.com>
2020-12-23 10:41:30 +0100

    fixture + delete figure + connexion

--- migrations/Version20201221085349.php
+++ migrations/Version20201221085349.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201218090055 extends AbstractMigration
+final class Version20201221085349 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- src/DataFixtures/FigureFixtures.php
+++ src/DataFixtures/FigureFixtures.php
@@ -25,47 +25,47 @@ class FigureFixtures extends Fixture
                 $tricksGroup->setName("Avancé");
             }
             $manager->persist($tricksGroup);
-        }
 
-        for ($k = 1; $k <= mt_rand(2, 4); $k++) {
-            $user = new User();
-            $user->setLogin("user n°$k")
-                ->setPassword("1234")
-                // ->setImages($image)
-                ->setMail("user$k@gmail.com");
-            $manager->persist($user);
-        }
-        for ($j = 1; $j <= mt_rand(4, 6); $j++) {
-            $figure = new Figure();
-            $figure->setName("figure n°$j")
-                ->setDescription("description n°$j")
-                ->setUser($user)
-                ->setCreatedAt(new \DateTime())
-                ->setTricksGroup($tricksGroup);
-            $manager->persist($figure);
-        }
+            for ($k = 1; $k <= 3; $k++) {
+                $user = new User();
+                $user->setLogin("user n°$k")
+                    ->setPassword("1234")
+                    // ->setImages($image)
+                    ->setMail("user$k@gmail.com");
+                $manager->persist($user);
+            }
+            for ($j = 1; $j <= 5; $j++) {
+                $figure = new Figure();
+                $figure->setName("figure n°$j")
+                    ->setDescription("description n°$j")
+                    ->setUser($user)
+                    ->setCreatedAt(new \DateTime())
+                    ->setTricksGroup($tricksGroup);
+                $manager->persist($figure);
+            }
 
-        for ($m = 1; $m <= mt_rand(3, 7); $m++) {
-            $image = new Images();
-            $image->setFigure($figure)
-                ->setLink("https://www.activeoutdoorpursuits.com/wp-content/uploads/2019/01/Active-Outdoor-Pursuits-Snowboarding-AOP-website-banner.jpg");
-            $manager->persist($image);
-        }
+            for ($m = 1; $m <= mt_rand(3, 15); $m++) {
+                $image = new Images();
+                $image->setFigure($figure)
+                    ->setLink("https://www.activeoutdoorpursuits.com/wp-content/uploads/2019/01/Active-Outdoor-Pursuits-Snowboarding-AOP-website-banner.jpg");
+                $manager->persist($image);
+            }
 
-        for ($l = 1; $l <= mt_rand(3, 7); $l++) {
-            $video = new Videos();
-            $video->setFigure($figure)
-                ->setLink("https://www.youtube.com/watch?v=8AWdZKMTG3U");
-            $manager->persist($video);
-        }
+            for ($l = 1; $l <= mt_rand(3, 10); $l++) {
+                $video = new Videos();
+                $video->setFigure($figure)
+                    ->setLink("https://www.youtube.com/watch?v=8AWdZKMTG3U");
+                $manager->persist($video);
+            }
 
-        for ($n = 1; $n <= mt_rand(3, 7); $n++) {
-            $comment = new Comment();
-            $comment->setContent("commentaire n°$n")
-                ->setUser($user)
-                ->setFigure($figure)
-                ->setCreatedAt(new \DateTime());
-            $manager->persist($comment);
+            for ($n = 1; $n <= mt_rand(3, 7); $n++) {
+                $comment = new Comment();
+                $comment->setContent("commentaire n°$n")
+                    ->setUser($user)
+                    ->setFigure($figure)
+                    ->setCreatedAt(new \DateTime());
+                $manager->persist($comment);
+            }
         }
         $manager->flush();
     }
